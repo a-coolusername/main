8216d69988a680865ca0b620d660ef7217250a8c
a-coolusername <ahmadelasir@gmail.com>
2024-08-15 12:15:35 +0300

    removed redundant article.save()

--- main/views.py
+++ main/views.py
@@ -138,13 +138,11 @@ def saveupdatearticle(request, id):
         new_tags = request.POST.getlist('tagschosen')
         if new_article_title:
             article.title = new_article_title
-            article.save()
         if new_content:
             article.content = new_content
-            article.save()
         if new_image:
             article.image = new_image
-            article.save()
         if new_tags:
             article.tags_associated.set(new_tags)
+        article.save()
     return redirect('/main/ArticleView/')
